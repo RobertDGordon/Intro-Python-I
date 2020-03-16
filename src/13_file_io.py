928cbe7dbf241138d4349d8262e664b5ae938f44
Robert Gordon <Komeuppance@gmail.com>
2020-03-16 00:21:33 -0700

    13-15 complete, added linter

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,28 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+import os.path
+
+with open(os.path.dirname(__file__) + '/foo.txt') as foo:
+    read_data = foo.read()
+    print(read_data)
+
+print(foo.closed)
+
+
+# f = open(os.path.dirname(__file__) + '/foo.txt')
+# print(f)
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+bar = open(os.path.dirname(__file__) + '/bar.txt', 'w')
+bar.write('Umm... is this going to work?\n')
+bar.write('Only one way to find out...\n')
+bar.write('Yippe kai yay!\n')
+bar.close()
+print(bar.closed)
